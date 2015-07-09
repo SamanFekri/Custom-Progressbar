491d6b1499c70dc5b83cb31c2b8de4b324a51358
SamanFekri <samanf74@gmail.com>
2015-07-09 09:39:26 +0430

    dynamically fill progressbar
    fill progressbar dynamically with asynctask when click on start

--- MyProgressBar/src/ir/skings/myprogressbar/MainActivity.java
+++ MyProgressBar/src/ir/skings/myprogressbar/MainActivity.java
@@ -1,5 +1,6 @@
 package ir.skings.myprogressbar;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -15,7 +16,8 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements android.view.View.OnClickListener{
+public class MainActivity extends Activity implements
+		android.view.View.OnClickListener {
 
 	private Handler progressBarHandler = new Handler();
 	private ProgressBar progressbar;
@@ -27,41 +29,48 @@ public class MainActivity extends Activity implements android.view.View.OnClickL
 
 		progressbar = (ProgressBar) findViewById(R.id.progressBar1);
 		progressbar.setMax(1000);
-		
+
 		Button button = (Button) findViewById(R.id.button1);
 		button.setOnClickListener(this);
-	}
-	
-	public void start(){
-		
-		Toast.makeText(getApplicationContext(), "hi", 0).show();
-		this.runOnUiThread(new Runnable() {
-			
-			int pos = 0;
-			
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-//				while (pos < 1000) {
-					pos += 10;
-					progressbar.setProgress(pos);
-//					try {
-//						Thread.sleep(50);
-//					} catch (InterruptedException e) {
-//						// TODO Auto-generated catch block
-//						e.printStackTrace();
-//					}
-//				}
-			}
-		});
 	}
 
 	@Override
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
-		start();
-		
+		new LongOperation().execute("");
+
 	}
-	
+
+	private class LongOperation extends AsyncTask<String, Void, String> {
+
+		int pos = 0;
+		
+		@Override
+		protected String doInBackground(String... params) {
+			while (pos < 1000) {
+				try {
+					pos += 10;
+					onProgressUpdate();
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					Thread.interrupted();
+				}
+			}
+			return "Executed";
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+		}
+
+		@Override
+		protected void onPreExecute() {
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+			progressbar.setProgress(pos);
+		}
+	}
 
 }
